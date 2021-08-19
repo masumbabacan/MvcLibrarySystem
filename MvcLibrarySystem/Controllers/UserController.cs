3a9432f18f489a59b3df2568fd420bef48e9ae63
= <masum1cocuxx@gmail.com>
2021-08-19 13:53:35 +0300

    19-08-2021

--- MvcLibrarySystem/Controllers/UserController.cs
+++ MvcLibrarySystem/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +13,9 @@ namespace MvcLibrarySystem.Controllers
     {
         // GET: User
         UserManager userManager = new UserManager(new EfUserDal());
-        public ActionResult Index()
+        public ActionResult Index(int page = 1)
         {
-            var result = userManager.GetAll();
+            var result = userManager.GetAll().ToPagedList(page,6);
             return View(result);
         }
 
