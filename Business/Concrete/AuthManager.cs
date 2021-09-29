e41cb538a7590efcab7799477053604929bdcddd
masumbabacan <78920463+masumbabacan@users.noreply.github.com>
2021-09-29 13:59:03 +0300

    Login İşlemi

---
+++ Business/Concrete/AuthManager.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class AuthManager : IAuthService
+    {
+        IUserDal _userDal;
+
+        public AuthManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public User GetUser(string email, string password)
+        {
+            return _userDal.Get(x => x.Email == email && x.Password == password);
+        }
+    }
+}
--- MvcLibrarySystem/Controllers/LoginController.cs
+++ MvcLibrarySystem/Controllers/LoginController.cs
@@ -6,17 +6,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MvcLibrarySystem.Controllers
 {
     public class LoginController : Controller
     {
         UserManager userManager = new UserManager(new EfUserDal());
-        // GET: Login
+
+        AuthManager authManager = new AuthManager(new EfUserDal());
+
+        [HttpGet]
         public ActionResult SignIn()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult SignIn(User user)
+        {
+            //bilgiler
+            var informations = authManager.GetUser(user.Email, user.Password);
+
+            if (informations != null)
+            {
+                FormsAuthentication.SetAuthCookie(informations.Email, false);
+                return RedirectToAction("Index", "Panel");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+
+
+
+
+
 
         [HttpGet]
         public ActionResult SignUp()
@@ -32,7 +59,7 @@ namespace MvcLibrarySystem.Controllers
                 return View("Kayit");
             }
             userManager.Add(user);
-            return View();
+            return RedirectToAction("Index", "HomePage");
         }
     }
 }
